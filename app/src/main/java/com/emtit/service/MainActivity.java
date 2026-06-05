c875821bb13ed8db3ce16bba30d004766d795982
mishkison <emishkis@gmail.com>
2026-06-05 11:15:50 +0300

    Update MainActivity.java

--- app/src/main/java/com/emtit/service/MainActivity.java
+++ app/src/main/java/com/emtit/service/MainActivity.java
@@ -2,9 +2,8 @@ package com.emtit.service;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.net.Uri;
 import android.widget.Button;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,20 +14,13 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnNewTicket = findViewById(R.id.btnNewTicket);
         Button btnCall1 = findViewById(R.id.btnCall1);
-        Button btnCall2 = findViewById(R.id.btnCall2);
 
         btnNewTicket.setOnClickListener(v ->
             startActivity(new Intent(MainActivity.this, NewTicketActivity.class)));
 
         btnCall1.setOnClickListener(v -> {
             Intent intent = new Intent(Intent.ACTION_DIAL);
-            intent.setData(android.net.Uri.parse("tel:035235989"));
-            startActivity(intent);
-        });
-
-        btnCall2.setOnClickListener(v -> {
-            Intent intent = new Intent(Intent.ACTION_DIAL);
-            intent.setData(android.net.Uri.parse("tel:0509166011"));
+            intent.setData(Uri.parse("tel:0509166011"));
             startActivity(intent);
         });
     }
